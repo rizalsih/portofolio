d3cd4dd994bcd1f71b9c7cd15198f18a23aa7764
rizalsih <abdulrizalsy@gmail.com>
2022-12-04 00:29:59 +0700

    pertama

---
+++ bot.js
@@ -0,0 +1,12 @@
+const pertanyaan = document.getElementById ("pertanyaan")
+const jawaban = document.getElementById ("jawaban")
+
+let init = 0
+
+const botSay = () => {
+    return ["halo nama saya rizalbot, nama kamu siapa?"]
+}
+
+function botStart () {
+console.log ("startig bot...")
+}
